3f011247ccf8f41dddb43b2bc235c75cf6c431aa
Mjaffres <maximilien.jaffres@gmail.com>
2017-03-29 16:25:47 +0200

    Add files via upload

--- IC3D.js
+++ IC3D.js
@@ -1,16 +1,39 @@
-//Hello
 var Alti=new Array();
 var LatLong=new Array();
 var ListeAlti=new Array();
-var Nbl=17;
-var Nbc=17;
+var listeRegion = new Array();
+var listeTuiles = new Array();
+var NewListeAlti = new Array();
+var Nbl=15;
+var Nbc=15;
 var controls;
 var clock = new THREE.Clock();
-var Max = -10000;
+var maxalti = -10000;
 
-var map = L.map('map').setView([48.629412,-4.529276],15);
+
+var map = L.map('map').setView([51.134642,1.358618],15);
 L.control.scale().addTo(map);
 L.tileLayer('https://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png').addTo(map);
+
+//POYAVE 1
+// Add it to the map
+//var areaSelect = L.areaSelect({width:200, height:300});
+//areaSelect.addTo(map);
+
+// Read the bouding box
+//var bounds = areaSelect.getBounds();
+
+// Get a callback when the bounds change
+//areaSelect.on("change", function() {
+//	console.log("Bounds:", this.getBounds());
+//});
+
+// Set the dimensions of the box
+//areaSelect.setDimensions({width: 500, height: 500})
+
+// And to remove it do:
+//areaSelect.remove();
+
 
 //POYAVE2
 var locationFilter = new L.LocationFilter().addTo(map);
@@ -39,21 +62,77 @@ locationFilter.on("disabled", function () {
 	// Do something when disabled.
 });
 
-function Mark1() {
-
+function Region (){
+	var compteurligne = 0;
+	var compteurcolonne = 0;
+	var maillelong = 0.0018;
+	var maillelat = maillelong/2 ;
 	var NE=Rec[0];
 	var NW=Rec[1];
 	var SW=Rec[2];
 	var SE=Rec[3];
+	console.log(NE);
+	listeRegion = [];
+	// bon, pas le pole nord (changement de dates)
+	for (var j = NW[1],finlongitude = NE[1]; j < finlongitude+maillelong; j = j+maillelong) {
+		compteurcolonne=compteurcolonne+1
+		compteurligne=0
+		for (var k = NW[0],finlatitude = SE[0]; k > finlatitude-maillelat; k = k-maillelat) {
+			compteurligne=compteurligne+1
+			console.log("coucouregion")
+			if (j < NE[1] ) {  longitudetemp=j}
+			else { longitudetemp=NE[1],console.log("finmaillelong")}
+			if (k > SE[0] ) {  latitudetemp=k}
+			else { latitudetemp=SE[0],console.log("finmaillelat") }
+			console.log(longitudetemp);
+			console.log(latitudetemp);
+			listeRegion.push([latitudetemp,longitudetemp]);
+			//L.marker([latitudetemp,longitudetemp]).addTo(map);
+		}
+	}
+	console.log(listeRegion);
+	nbmaille = (compteurcolonne-1)*(compteurligne-1);
+	console.log(nbmaille,compteurcolonne,compteurligne);
+	return([nbmaille,compteurligne,compteurcolonne]);
+}
+
+
+function Mark2() {
+	NewListeAlti = [];
+	var format = Region();
+	var listeTuiles = [];
+	console.log(format);
+	for (var k = 0,fink = format[1]*format[2]-format[1]; k < fink; k++) {
+		if ((k+1)%format[1]==0) { console.log('je passe le point')}
+		else {
+			//L.marker([listeRegion[k][0],listeRegion[k][1]]).addTo(map);
+			L.marker([listeRegion[k+format[1]+1][0],listeRegion[k+format[1]+1][1]]).addTo(map);
+			console.log('markerpls');
+			listeTuiles.push([Mark1(listeRegion[k],listeRegion[k+format[1]+1])]);
+			console.log(listeTuiles);
+			}
+
+
+
+
+	}
+   }
+
+
+
+
+function Mark1(NW,SE) {
+
+
+
 	Alti=[];
 	LatLong=[];
 	ListeAlti=[];
-	var C=NW[0]-SW[0];
-	var L=NE[1]-NW[1];
+	var C=Math.abs(NW[0]-SE[0]);
+	var L=Math.abs(SE[1]-NW[1]);
 	var dc=L/Nbc;
 	var dl=C/Nbl;
 	var PP="";
-
 
 	for (var j = 0,len0 = Nbl; j < len0; j++) {
 
@@ -70,7 +149,7 @@ function Mark1() {
 	}
 
 	PP=PP.substring(0,PP.length-3);
-	var url="https://maps.googleapis.com/maps/api/elevation/json?locations="+PP+"&key=AIzaSyCTSax3v-9bQ0gzolfK22QqJPSmQyMcdjA";
+	var url="https://maps.googleapis.com/maps/api/elevation/json?locations="+PP+"&key=AIzaSyAy2lw1yp2qpd9SgPJxNOlAHJZcUs7YgYc";
 	var yql_url = 'https://query.yahooapis.com/v1/public/yql';
 
 	$.ajax({
@@ -88,33 +167,46 @@ function Mark1() {
 			}
 
 			for (var k=0,len2=Nbc*Nbl;k<len2;k++) {
-				ListeAlti.push(Elevation[k].elevation)
-				if (Elevation[k].elevation > Max){
-					Max = Elevation[k].elevation;
+				ListeAlti.push(Elevation[k].elevation);
+				if (Elevation[k].elevation > maxalti){
+					maxalti = Elevation[k].elevation;
 				}
-
 			}
 			console.log(ListeAlti);
 
 		},
 
-		error: function (data) {
+		error: function (response) {
 			alert("Error");
 		}
 	});
 
-	return(Max);
-
+	return(ListeAlti)
 }
 
 
-//"https://api.mapbox.com/styles/v1/mapbox/satellite-v9/static/"+LatLong[1]+","+LatLong[0]+",3/600x300?access_token=sk.eyJ1Ijoia3l6ZXJzdXNoaSIsImEiOiJjaXpoZ25tdnIwMDNqMndsOTB4NmdoNHExIn0.eemm2BU0LXva8-vXGGWAAA"
+
 
 function Affichage() {
 
-		console.log('coucou')
-		console.log(Max)
-		console.log(ListeAlti)
+		Format=Region();
+		//return([nbmaille,compteurcolonne,compteurligne]);
+		var tempArray = new Array();
+
+
+		for (var k = 0,lenAlti = ListeAlti.length; k < lenAlti; k++) {
+
+			tempArray.push(ListeAlti[k]);
+
+			if ((k+1)%(Nbc*Nbl)==0) {
+				NewListeAlti.push(tempArray);
+				tempArray = [];
+
+			}
+		}
+
+		console.log(NewListeAlti);
+
 		console.log("coucouaffichage")
 		var renderer = new THREE.WebGLRenderer();
 		renderer.setSize( window.innerWidth, window.innerHeight );
@@ -123,73 +215,88 @@ function Affichage() {
 
 		var scene = new THREE.Scene();
 
-		var camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 1, 500);
-		camera.position.set(0, 0, Max);
+		var camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 1, 1500);
+		camera.position.set(0, 0, maxalti);
 
 
-		var geometry2 = new THREE.BoxGeometry(1, 1, 1 );
+		var material = new THREE.MeshBasicMaterial( { color: 0x999999, wireframe : true } );
 		var geometry = new THREE.PlaneGeometry(60,60,Nbl-1, Nbc-1);
+		var geometry2 = new THREE.BoxGeometry(1, 1, 1 );
+
 		console.log(geometry.vertices.length)
-		for (var i = 0, l = geometry.vertices.length; i < l; i++) {
-		  geometry.vertices[i].z = ListeAlti[i] ;
-			console.log(ListeAlti[i])
-		}
+		var compteur=0;
+		var Tableau_scene = new Array();
+		for (var j = 0, fin = Format[1]-1 ; j < fin; j++) {
+			for (var k = 0, fin2 = Format[2]-1 ; k < fin2; k++) {
+				console.log('meurs')
+				if (compteur < NewListeAlti.length) {
+
+					var geometry = new THREE.PlaneGeometry(60,60,Nbl-1, Nbc-1);
+					console.log(compteur);
+
+					for (var y = 0, len = geometry.vertices.length; y < len; y++) {
 
 
 
-		var material = new THREE.MeshBasicMaterial( {
-			color: 0xdddddd ,
-			wireframe : true
-		} );
+						geometry.vertices[y].z = NewListeAlti[compteur][y] ;
+						console.log(NewListeAlti[compteur][y])
+						console.log(compteur);
+						console.log(y);
+
+						//var plane_+String(compteur) = new THREE.Mesh(geometry, material);
+						//scene.add(plane);
+					}
+
+					var material1 = new THREE.MeshBasicMaterial( { color: 0xFF0000, wireframe : true } );
+					var material2 = new THREE.MeshBasicMaterial( { color: 0x0000FF, wireframe : true } );
+					var material3 = new THREE.MeshBasicMaterial( { color: 0x00FF00, wireframe : true } );
+					var material4 = new THREE.MeshBasicMaterial( { color: 0xD2691E, wireframe : true } );
+					if (compteur == 0){
+						Tableau_scene.push(new THREE.Mesh(geometry, material1));
+					}
+					else if (compteur == 1) {
+						Tableau_scene.push(new THREE.Mesh(geometry, material2));
+					}
+					else if	(compteur == 2){
+						Tableau_scene.push(new THREE.Mesh(geometry, material3));
+					}
+					else if (compteur == 3){
+						Tableau_scene.push(new THREE.Mesh(geometry, material4));
+					}
+					else {
+						Tableau_scene.push(new THREE.Mesh(geometry, material));
+					}
+
+					console.log(Tableau_scene);
+
+					Tableau_scene[compteur].position.x = Tableau_scene[compteur].position.x-k*60;
+					Tableau_scene[compteur].position.y = Tableau_scene[compteur].position.y+j*60;
+					Tableau_scene[compteur].rotation.z = +90*2*Math.PI/360 ;
+					compteur=compteur+1;
+				}
 
 
-		var central = new Array((Rec[0][0]+Rec[1][0]+Rec[2][0]+Rec[3][0])/4,((Rec[0][1]+Rec[1][1]+Rec[2][1]+Rec[3][1])/4));
-		//central[0]=(Rec[0][0]+Rec[1][0]+Rec[2][0]+Rec[3][0])/4;
-		//central[1]=(Rec[0][1]+Rec[1][1]+Rec[2][1]+Rec[3][1])/4;
-
-		var texture = new THREE.TextureLoader();
-		texture.crossOrigin = 'anonymous';
-		//texture.load("https://api.mapbox.com/styles/v1/mapbox/satellite-v9/static/"+central[0]+","+central[1]+",5/256x128?access_token=sk.eyJ1Ijoia3l6ZXJzdXNoaSIsImEiOiJjaXpoZ25tdnIwMDNqMndsOTB4NmdoNHExIn0.eemm2BU0LXva8-vXGGWAAA");
-		var textureMaterial = new THREE.MeshLambertMaterial({ color: 0xdddddd,
-		wireframe: true});
-
-		var texturechat = new THREE.TextureLoader().load("chat.jpg");
-
-		// load a resource
-		texture.load("https://api.mapbox.com/styles/v1/mapbox/satellite-v9/static/"+central[1]+","+central[0]+",17/1024x1024?access_token=sk.eyJ1Ijoia3l6ZXJzdXNoaSIsImEiOiJjaXpoZ25tdnIwMDNqMndsOTB4NmdoNHExIn0.eemm2BU0LXva8-vXGGWAAA",
-		// Function when resource is loaded
-				function ( texture ) {
-					var textureMaterial = new THREE.MeshBasicMaterial({ map: texture });
-					console.log("Victory is mine")
-					var plane = new THREE.Mesh(geometry, textureMaterial);
-					scene.add( plane );
-					plane.rotation.x += -90*2*Math.PI/360;
-					//plane.rotation.z += 45*Math.PI/360
-				},
-				// Function called when download progresses
-				function ( xhr ) {console.log( (xhr.loaded / xhr.total * 100) + '% loaded' )},
-				// Function called when download errors
-				function ( xhr ) {console.log('');
 			}
-		);
+		}
+		console.log(Tableau_scene);
+		for (var m=0,len = Tableau_scene.length; m<len ;m++){
+			scene.add(Tableau_scene[m]);
+			console.log("Dessin")
+		};
 
 
 
-		//https://api.mapbox.com/styles/v1/mapbox/satellite-v9/static/2.6,48.8,5/256x128?access_token=sk.eyJ1Ijoia3l6ZXJzdXNoaSIsImEiOiJjaXpoZ25tdnIwMDNqMndsOTB4NmdoNHExIn0.eemm2BU0LXva8-vXGGWAAA
+		//for (var i = 0, l = geometry.vertices.length; i < l; i++) {
+		//geometry.vertices[i].z = ListeAlti[i] ;
+		//	console.log(ListeAlti[i])
+		//}
 
-		var material2 = new THREE.MeshBasicMaterial({
-		color: 0xdddddd,
-		wireframe: true
-		});
+		//var plane = new THREE.Mesh(geometry, material);
+		//scene.add(plane);
 
-
-
-
-
-		var cube = new THREE.Mesh( geometry2, material2 );
+		var cube = new THREE.Mesh( geometry2, material );
 		scene.add( cube );
-
-		scene.add(new THREE.AmbientLight(0xeeeeee));
+		console.log("Wurst")
 
 
 		// Axe du dessin
@@ -198,9 +305,7 @@ function Affichage() {
 
 		var AxeX= new THREE.Geometry();
 		var AxeY= new THREE.Geometry();
-		var AxeZ= new THREE.Geometry();
-
-		/*
+		var AxeZ= new THREE.Geometry();/*
 		AxeX.vertices.push(new THREE.Vector3(0,0,0));
 		AxeY.vertices.push(new THREE.Vector3(0,0,0));
 		AxeZ.vertices.push(new THREE.Vector3(0,0,0));
@@ -225,7 +330,7 @@ function Affichage() {
 		geometry2.vertices.push(new THREE.Vector3(0,70,0));
 		geometry2.vertices.push(new THREE.Vector3(70,0,0));
 
-		var material = new THREE.MeshBasicMaterial( { color: 0xdddddd ,
+		var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 ,
 		wireframe: true} );
 
 		var line = new THREE.Line(geometry2, material)
